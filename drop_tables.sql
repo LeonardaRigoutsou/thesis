41244dd78b67f764a0498aa1490cdfc0fb8ce861
LeonardaRigoutsou <elinarigoutsou@gmail.com>
2022-11-06 22:17:40 +0200

    Added a script for dropping the db tables.

---
+++ drop_tables.sql
@@ -0,0 +1,7 @@
+drop table if exists categories cascade;
+drop table if exists items cascade;
+drop table if exists orderitems cascade;
+drop table if exists orders cascade;
+drop table if exists reservations cascade;
+drop table if exists tables cascade;
+drop table if exists users cascade;
