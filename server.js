3288dc4e71cf562bb588bf68a32697b2ddfaee36
LeonardaRigoutsou <elinarigoutsou@gmail.com>
2022-11-06 22:16:56 +0200

    introducing postgres dependencies, created database schema using sequelize and implemented the database's connection with the server.

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const swagger = require('swagger-ui-express');
 
+const db = require('./util/database');
+
 const swaggerConfig = require('./swagger.json');
 
 const app = express();
 
 app.use('/api-docs', swagger.serve, swagger.setup(swaggerConfig));
 
+db.sequelize.authenticate().then(() => {
+    console.log('Connection has been established successfully.');
+}).catch((error) => {
+    console.error('Unable to connect to the database: ', error);
+});
 
-app.listen(8080, 'localhost');
+db.sequelize
+    .sync({ force: true })
+    .then(result => {
+        app.listen(8080, 'localhost');
+    })
+    .catch(err => {
+        console.log(err);
+    })
---
+++ util/database.js
@@ -0,0 +1,215 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('postgres://rykrhuky:5OVYPkBqUxPjTWoWfLa6Kk4pmVgediTi@tyke.db.elephantsql.com/rykrhuky', {});
+
+const Category = sequelize.define('categories', {
+    categoryId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        unique: true
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    isAvailable: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false
+    }
+}, { "timestamps": false });
+
+const Order = sequelize.define('orders', {
+    orderId: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+        unique: true
+    },
+    serverId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    tableNum: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    orderDate: {
+        type: Sequelize.DATE,
+        allowNull: false
+    },
+    state: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    instructions: {
+        type: Sequelize.STRING,
+        allowNull: true
+    }
+}, { "timestamps": false });
+
+const Item = sequelize.define('items', {
+    itemId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        unique: true
+    },
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    price: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    isAvailable: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+    },
+    ingredients: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+}, { "timestamps": false });
+
+const OrderItem = sequelize.define('orderitems', {
+    // orderId: {
+    //     type: Sequelize.INTEGER,
+    //     allowNull: false,
+    //     references: {
+    //         model: Order,
+    //         key: 'orderId'
+    //     },
+    //     primaryKey: true,
+    //     unique: true
+    // },
+    // itemId: {
+    //     type: Sequelize.INTEGER,
+    //     allowNull: false,
+    //     references: {
+    //         model: Item,
+    //         key: 'itemId'
+    //     },
+    //     primaryKey: true,
+    //     unique: true
+    // },
+    status: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    quantity: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    qualifiers: {
+        type: Sequelize.STRING,
+        allowNull: true
+    }
+}, { "timestamps": false });
+
+const Reservation = sequelize.define('reservations', {
+    reservationId: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+        unique: true
+    },
+    reservationName: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    reservationDate: {
+        type: Sequelize.DATE,
+        allowNull: false
+    }
+}, { "timestamps": false });
+
+const Table = sequelize.define('tables', {
+    tableNum: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        unique: true
+    },
+    seats: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    locationX: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    locationY: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    }
+}, { "timestamps": false });
+
+const User = sequelize.define('users', {
+    userId: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+        unique: true
+    },
+    username: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    firstName: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    lastName: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    hireDate: {
+        type: Sequelize.DATE,
+        allowNull: false
+    },
+    role: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+}, { "timestamps": false });
+
+//Describing associations with foreign keys
+User.hasOne(Order, {
+    foreignKey: "serverId"
+});
+Order.belongsTo(Table, {
+    foreignKey: "tableNum"
+});
+
+Reservation.belongsTo(Table, {
+    foreignKey: "tableNum"
+});
+
+Order.belongsToMany(Item, {
+    through: OrderItem,
+    foreignKey: 'orderId'
+});
+Item.belongsToMany(Order, {
+    through: OrderItem,
+    foreignKey: 'itemId'
+});
+
+Item.belongsTo(Category, {
+    foreignKey: "categoryId"
+});
+
+
+
+
+exports.sequelize = sequelize;
