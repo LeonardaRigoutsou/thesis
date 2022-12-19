8f4764c670d79d2e1f201227134dd9a359b812fe
LeonardaRigoutsou <elinarigoutsou@gmail.com>
2022-12-19 23:12:46 +0200

    Fixed authorization role to cooker.

--- backend/routes/orderroutes.js
+++ backend/routes/orderroutes.js
@@ -6,12 +6,12 @@ const { route } = require('./authroutes');
 
 const router = express.Router();
 
-router.get('/orders', authenticate, authorize(['cook', 'admin']), orderController.getOrders);
-router.get('/order/:id', authenticate, authorize(['server', 'cook', 'admin']), orderController.getOrderById);
+router.get('/orders', authenticate, authorize(['cooker', 'admin']), orderController.getOrders);
+router.get('/order/:id', authenticate, authorize(['server', 'cooker', 'admin']), orderController.getOrderById);
 
 router.post('/order', authenticate, authorize(['server']), orderController.createOrder);
 
-router.put('/order/:id', authenticate, authorize(['server', 'cook']), orderController.updateOrder);
+router.put('/order/:id', authenticate, authorize(['server', 'cooker']), orderController.updateOrder);
 
 
 module.exports = router;
