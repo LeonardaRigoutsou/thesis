854163a57d646d58c77214761606f2e132623b6e
LeonardaRigoutsou <elinarigoutsou@gmail.com>
2023-08-15 17:30:38 +0300

    implementing the dynamic map placement on the server map page

--- web/src/app/components/table-button/table-button.component.ts
+++ web/src/app/components/table-button/table-button.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { Table } from 'src/app/services/table.service';
 
 @Component({
   selector: 'table-button',
@@ -7,4 +9,15 @@ import { Component, Input } from '@angular/core';
 })
 export class TableButtonComponent {
   @Input() img: string = '';
+  @Input() table: Table;
+
+  constructor(private router: Router) { }
+
+  onTableClick(tableId: number) {
+    this.router.navigate(['server', 'order'], {
+      queryParams: {
+        table: tableId
+      }
+    });
+  }
 }
--- web/src/app/pages/server-map-page/server-map-page.component.ts
+++ web/src/app/pages/server-map-page/server-map-page.component.ts
@@ -1,25 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ViewContainerRef, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { DragableIconComponent } from 'src/app/components/dragable-icon/dragable-icon.component';
+import { TableButtonComponent } from 'src/app/components/table-button/table-button.component';
 import { AuthService } from 'src/app/services/auth.service';
+import { Table, TableService } from 'src/app/services/table.service';
 
 @Component({
   selector: 'app-server-map-page',
   templateUrl: './server-map-page.component.html',
   styleUrls: ['./server-map-page.component.css']
 })
-export class ServerMapPageComponent {
+export class ServerMapPageComponent implements OnInit {
 
-  constructor(private authService: AuthService, private router: Router) { }
+  tables: Table[] = [];
+  @ViewChild('tables', { read: ViewContainerRef }) mapContainerRef: ViewContainerRef;
+
+  constructor(private authService: AuthService, private tableService: TableService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.tableService.getTables().subscribe({
+      next: (response) => {
+        this.tables = response.tables;
+      },
+      error: (error) => {
+        console.log(error);
+      },
+      complete: () => {
+        this.placeTables();
+      }
+    });
+  }
 
   onLogout() {
     this.authService.logout();
   }
 
-  onTableClick(tableId: number) {
-    this.router.navigate(['server', 'order'], {
-      queryParams: {
-        table: tableId
-      }
-    });
+  placeTables() {
+    this.tables.forEach(table => {
+      let tableRef = this.mapContainerRef.createComponent(TableButtonComponent);
+      tableRef.instance.table = table;
+    })
   }
 }
